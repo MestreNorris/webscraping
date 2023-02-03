f84194d05d6797aa9ebd95f40dea881d998ef05e
Alan Braun Pereira <alanbraunpereira@hotmail.com>
2023-02-03 16:16:27 -0300

    update error db

--- src/pages/filmes.js
+++ src/pages/filmes.js
@@ -1,19 +1,12 @@
 import Card from '../components/card/index.js'
+import data from '../pages/api/jsonAnimes.json'
 
 const Filmes = ({ data }) => {
   return <Card data={data} />
 }
 
 export async function getStaticProps() {
-  const res = await fetch('https://webscraping.vercel.app/api/filmes')
-  const filmesData = await res.json()
-
-  if (!res.ok) throw new Error('Erro ao realizar a requisição')
-
-  return {
-    props: { data: filmesData.data },
-    revalidate: 60
-  }
+  return { props: { data } }
 }
 
 export default Filmes
--- src/pages/series.js
+++ src/pages/series.js
@@ -1,19 +1,12 @@
 import Card from '../components/card/index.js'
+import data from '../pages/api/jsonAnimes.json'
 
 const Series = ({ data }) => {
   return <Card data={data} />
 }
 
-export default Series
-
 export async function getStaticProps() {
-  const res = await fetch('https://webscraping.vercel.app/api/series')
-  const seriesData = await res.json()
-
-  if (!res.ok) throw new Error('Erro ao realizar a requisição')
-
-  return {
-    props: { data: seriesData.data },
-    revalidate: 60
-  }
+  return { props: { data } }
 }
+
+export default Series
