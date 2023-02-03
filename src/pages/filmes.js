98de857f55a6628d75ea189f3403b08f06ab5ee2
Alan Braun Pereira <alanbraunpereira@hotmail.com>
2023-02-03 16:19:17 -0300

    update filmes

--- src/pages/filmes.js
+++ src/pages/filmes.js
@@ -1,12 +1,19 @@
 import Card from '../components/card/index.js'
-import data from '../pages/api/jsonAnimes.json'
 
 const Filmes = ({ data }) => {
   return <Card data={data} />
 }
 
 export async function getStaticProps() {
-  return { props: { data } }
+  const res = await fetch('https://webscraping.vercel.app/api/filmes')
+  const filmesData = await res.json()
+
+  if (!res.ok) throw new Error('Erro ao realizar a requisição')
+
+  return {
+    props: { data: filmesData.data },
+    revalidate: 60
+  }
 }
 
 export default Filmes
