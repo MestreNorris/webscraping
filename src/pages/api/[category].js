6480f537ea70f5e197ccf34d6d714d2f86047d90
Alan Braun Pereira <alanbraunpereira@hotmail.com>
2023-02-03 20:28:49 -0300

    update api

--- src/pages/api/[category].js
+++ src/pages/api/[category].js
@@ -1,19 +1,20 @@
 import { executeOperation } from '../../components/connection/mongodb'
+import { Error404 } from '../404'
 
 const handler = async (req, res) => {
   try {
-    const { method } = req
     var t0 = performance.now()
-
-    const data = await executeOperation('animes', 'findAll')
-    var t1 = performance.now()
-    console.log('Operação completada em ' + ((t1 - t0) / 1000).toFixed(2) + ' segundos.')
-
-    console.log(data)
+    const { method, query } = req
+    const suportedAPIs = ['animes', 'series', 'filmes', 'mangas']
 
     switch (method) {
       case 'GET':
-        res.status(200).json({ data })
+        suportedAPIs.includes(query.category)
+          ? res.status(200).json(await executeOperation(query.category, 'findAll'))
+          : res.render(Error404)
+
+        var t1 = performance.now()
+        console.log('Operação completada em ' + ((t1 - t0) / 1000).toFixed(2) + ' segundos.')
         break
       default:
         res.setHeader('Allow', ['GET', 'POST'])
