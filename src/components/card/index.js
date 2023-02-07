4d94951faf96dac323892fd7dc9c062304e8fd87
Alan Braun Pereira <alanbraunpereira@hotmail.com>
2023-02-07 20:52:42 -0300

    update mangas

--- src/components/card/index.js
+++ src/components/card/index.js
@@ -47,12 +47,11 @@ const Cover = ({ src, type }) => {
   return (
     <Figure>
       <Image
-        src={srcImg} //src={isValidUrl(srcImg, type)}
+        src={isValidUrl(srcImg, type)}
         fill
         sizes='(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw'
+        onError={() => setSrcImg(typeCategory(type))}
         alt='Capa'
-
-        // onError={() => setSrcImg(typeCategory(type))}
       />
     </Figure>
   )
